9293dc4bfb9b6a62b139dbdc3e30ca2c2f28c13a
Levi McCallum <levi@levimccallum.com>
2026-09-17 01:44:19 -0700

    chore: adopt published runtime with scoped event identity

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let localRuntimePath = ".artifacts/NuxieRuntime.xcframework"
 let packageRoot = URL(fileURLWithPath: #filePath).deletingLastPathComponent()
 let localRuntimeURL = packageRoot.appendingPathComponent(localRuntimePath)
 let releasedRuntimeBaseURL = "https://github.com/nuxieai/nuxie-runtime/releases/download"
-let releasedRuntimeURL = releasedRuntimeBaseURL + "/apple-runtime-v0.9.14/NuxieRuntime.xcframework.zip"
-let releasedRuntimeChecksum = "af5bb6fbc55d30d25ecfb8940620b9c351857d620ecd64ab7c45ed38ec5d512b"
+let releasedRuntimeURL = releasedRuntimeBaseURL + "/apple-runtime-v0.10.0/NuxieRuntime.xcframework.zip"
+let releasedRuntimeChecksum = "a3131249683d51c99ad4d4891176e72079d8d07c46697b77909af4eaae1c00a4"
 
 func makeNuxieRuntimeBinaryTarget() -> Target {
     let localRuntimeSelection = ProcessInfo.processInfo.environment["NUXIE_RUNTIME_USE_LOCAL"]
