871f1501b123288e8f5805cc367bfb940f7036ee
Levi McCallum <levi@levimccallum.com>
2026-09-23 12:03:54 -0700

    chore: adopt Apple runtime 0.10.11 for input scrolling

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let localRuntimePath = ".artifacts/NuxieRuntime.xcframework"
 let packageRoot = URL(fileURLWithPath: #filePath).deletingLastPathComponent()
 let localRuntimeURL = packageRoot.appendingPathComponent(localRuntimePath)
 let releasedRuntimeBaseURL = "https://github.com/nuxieai/nuxie-runtime/releases/download"
-let releasedRuntimeURL = releasedRuntimeBaseURL + "/apple-runtime-v0.10.10/NuxieRuntime.xcframework.zip"
-let releasedRuntimeChecksum = "50c66c4d6e663f3a0ee2d24be2e83b0e3a65024bc067ab0bf13d9e53c0844c71"
+let releasedRuntimeURL = releasedRuntimeBaseURL + "/apple-runtime-v0.10.11/NuxieRuntime.xcframework.zip"
+let releasedRuntimeChecksum = "89b9d7cd181c30d71ac4cada73e8eb2cbadd45416c0f623c4dcc568aa7a5662e"
 
 func makeNuxieRuntimeBinaryTarget() -> Target {
     let localRuntimeSelection = ProcessInfo.processInfo.environment["NUXIE_RUNTIME_USE_LOCAL"]
